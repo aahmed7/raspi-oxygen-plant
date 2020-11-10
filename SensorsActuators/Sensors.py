8baf61a16de20c058f4fd14762d80a2c8ac0375d
aahmed7 <arslan.aa7@gmail.com>
2020-11-11 00:06:25 +0500

    initial commit

---
+++ SensorsActuators/Sensors.py
@@ -0,0 +1,42 @@
+import gpiozero
+# Sensors are read using ADS1115
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+import busio
+import board
+
+# Set GAIN to +/- 4.096V
+GAIN = 1
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Define address for ADCs
+ADC1 = 0x48
+ADC2 = 0x49
+
+# Create ADC objects
+adc1 = ADS.ADS1115(ADC1,i2c)
+adc2 = ADS.ADS1115(ADC2,i2c)
+
+# Define index for sensors
+PRESSURE1 = ADS.P0
+PRESSURE2 = ADS.P1
+PRESSURE3 = ADS.P2
+PRESSURE4 = ADS.P0
+PRESSURE5 = ADS.P1
+OXYGEN1   = ADS.P2
+
+class Sensors:
+    # Initialize the ADC
+    def __init__(self):
+        press1 = AnalogIn(adc1,PRESSURE1)
+        press2 = AnalogIn(adc1,PRESSURE2)
+        press3 = AnalogIn(adc1,PRESSURE3)
+        press4 = AnalogIn(adc2,PRESSURE4)
+        press5 = AnalogIn(adc2,PRESSURE5)
+        oxy1 = AnalogIn(adc2,OXYGEN1)
+
+    def readSensor(self, sensor_name):
+        return sensor_name.value
+        
---
+++ functions.py
@@ -0,0 +1,66 @@
+def read_pressure(sensor_index):
+    pass
+
+def activate_piston(piston_index):
+    pass
+
+def check_oxygen_purity():
+    pass
+
+def valve_pid_control(valve_index):
+    pass
+
+def adjust_pressure(tank_index):
+    pass
+
+def factory_reset():
+    pass
+
+def calibrate():
+    pass
+
+def fill_tank(tank_index):
+    pass
+
+def set_filling_time(tank_index):
+    pass
+
+def balance_tank():
+    pass
+
+def maintenance_reset():
+    pass
+
+def filter_life_alarm():
+    pass
+
+def oxygen_sensor_life_alarm():
+    pass
+
+def chemical_maintenance_time_alarm():
+    pass
+
+def test_valve():
+    pass
+
+def show_alarm_min_value():
+    pass
+
+def set_alarm_passive():
+    pass
+
+def error_time_alarm():
+    pass
+
+def set_master_password():
+    pass
+
+def set_user_password():
+    pass
+
+def master_login():
+    pass
+
+def user_login():
+    pass
+
